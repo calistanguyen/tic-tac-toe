653fbcf20be1832e548a22f8eb34d02846eb3b1c
John Griffin <John@Johns-MacBook-Air-2.local>
2020-09-23 17:22:22 -0600

    added PlayerChoice

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -10,14 +10,30 @@ void CreateBoard(char board[3][3]){
 void DisplayBoard(char board[3][3]){
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      std::cout << board[i][j] << ' \n'[j == 3-1];
+      std::cout << board[i][j];
     }
+    std::cout << '\n';
   }
-
 }
+
+ static int* GetPlayerChoice(){
+  std::string row, col;
+  static int cords[2];
+  std::cout << "Enter a row: " << '\n';
+  std::cin >> row;
+  int int_row = stoi(row);
+  cords[0] = int_row;
+  std::cout << "Enter a column: " << '\n';
+  std::cin >> col;
+  int int_col = stoi(col);
+  cords[1] = int_col;
+  return cords;
+}
+
 int main(){
-
-    char board[3][3]; //initializing 2d array for the board
-    CreateBoard(board);
-
+  int *ptr = GetPlayerChoice();
+  int location[2] = {ptr[0], ptr[1]};
+  char board[3][3]; //initializing 2d array for the board
+  CreateBoard(board);
+  DisplayBoard(board);
 }
