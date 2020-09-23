22d73f856f19708ee8652bae0d6b48345d5690b5
Calista Nguyen <nguyen.calista@gmail.com>
2020-09-23 15:39:28 -0600

    fixing display board

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -10,8 +10,9 @@ void CreateBoard(char board[3][3]){
 void DisplayBoard(char board[3][3]){
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      std::cout << board[i][j] << ' \n'[j == 3-1];
+      std::cout << board[i][j];
     }
+    std::cout << std::endl; 
   }
 
 }
