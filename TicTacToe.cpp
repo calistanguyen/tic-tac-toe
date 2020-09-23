f523a9eafe304f006a5b2adc578e4340220d4598
Calista Nguyen <nguyen.calista@gmail.com>
2020-09-23 15:22:33 -0600

    making createBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -8,9 +8,19 @@ void CreateBoard(char board[3][3]){
     }
 }
 
+void DisplayBoard(char board[3][3]){
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            std::cout << board[i][j]; 
+        }
+        std::cout << std::endl; 
+    }
+}
+
 int main(){
 
     char board[3][3]; //initializing 2d array for the board
     CreateBoard(board); 
+    DisplayBoard(board); 
 
 }
